e642ae68afea3f1511b45607a71707cfb8cab79e
Akbar1007 <a.akbarjon007@gmail.com>
2025-01-08 22:16:29 +0500

    Day 2

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -40,6 +40,20 @@ int main() {
     if (integer_3 > integer_1 && integer_3 > integer_2)
         cout << integer_3 << " is the largest one." << endl;
 
+    //Program 2:
+    cout << "Integer  Square  Cube" << endl;
+    // Using power function:
+    cout << pow(0, 1) << "        " << pow(0, 2) << "       " << pow(0, 3) << endl;
+    cout << pow(1, 1) << "        " << pow(1, 2) << "       " << pow(1, 3) << endl;
+    cout << pow(2, 1) << "        " << pow(2, 2) << "       " << pow(2, 3) << endl;
+    cout << pow(3, 1) << "        " << pow(3, 2) << "       " << pow(3, 3) << endl;
+    cout << pow(4, 1) << "        " << pow(4, 2) << "      " << pow(4, 3) << endl;
+    cout << pow(5, 1) << "        " << pow(5, 2) << "      " << pow(5, 3) << endl;
+    cout << pow(6, 1) << "        " << pow(6, 2) << "      " << pow(6, 3) << endl;
+    cout << pow(7, 1) << "        " << pow(7, 2) << "      " << pow(7, 3) << endl;
+    cout << pow(8, 1) << "        " << pow(8, 2) << "      " << pow(8, 3) << endl;
+    cout << pow(9, 1) << "        " << pow(9, 2) << "      " << pow(9, 3) << endl;
+    cout << pow(10, 1) << "       " << pow(10, 2) << "     " << pow(10, 3) << endl;
 
     return 0;
 }
