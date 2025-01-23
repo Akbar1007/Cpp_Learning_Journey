4c80e4196ab7d7a6c43ab873f9848f3f273ec6da
Akbar1007 <a.akbarjon007@gmail.com>
2025-01-23 13:11:40 +0500

    Day 7

--- Day07/program1.cpp
+++ Day07/program1.cpp
@@ -21,15 +21,15 @@ int main() {
 
     cout << "Enter length and width of the rectangle: ";
     cin >> length >> width;
-    cout << "Area of Rectangle: " << area(length, width) << endl;
+    cout << "Area of the Rectangle: " << area(length, width) << endl;
 
     cout << "Enter base and height of the triangle: ";
     cin >> base >> height;
-    cout << "Area of Triangle: " << area(base, height, 0) << endl;
+    cout << "Area of the Triangle: " << area(base, height, 0) << endl;
 
     cout << "Enter side of the square: ";
     cin >> side;
-    cout << "Area of Square: " << area(side) << endl;
+    cout << "Area of the Square: " << area(side) << endl;
 
     return 0;
 }
